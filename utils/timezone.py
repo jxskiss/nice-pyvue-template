b12d618c39f1d9a9cc14c6ad63206b9cb1133456
jxskiss <jxskiss@126.com>
2018-01-24 01:02:11 +0800

    Timezone related utilities borrowed from Django

---
+++ utils/timezone.py
@@ -0,0 +1,125 @@
+# -*- coding:utf-8 -*-
+from datetime import datetime, date, timedelta
+import dateutil.parser
+import os
+import pytz
+
+parse = dateutil.parser.parse
+
+
+def get_current_timezone():
+    tz = os.getenv('TZ', os.getenv('TIMEZONE', os.getenv('TIME_ZONE')))
+    if tz:
+        return pytz.timezone(tz)
+    return pytz.utc
+
+
+def localtime(value=None, timezone=None):
+    """
+    Converts an aware datetime.datetime to local time.
+
+    Only aware datetimes are allowed. When value is omitted, it defaults to
+    now().
+
+    Local time is defined by the current time zone, unless another time zone
+    is specified.
+    """
+    if value is None:
+        value = now()
+    if timezone is None:
+        timezone = get_current_timezone()
+    # Emulate the behavior of astimezone() on Python < 3.6.
+    if is_naive(value):
+        raise ValueError("localtime() cannot be applied to a naive datetime")
+    value = value.astimezone(timezone)
+    if hasattr(timezone, 'normalize'):
+        # This method is available for pytz time zones.
+        value = timezone.normalize(value)
+    return value
+
+
+def localdate(value=None, timezone=None):
+    """
+    Convert an aware datetime to local time and return the value's date.
+
+    Only aware datetimes are allowed. When value is omitted, it defaults to
+    now().
+
+    Local time is defined by the current time zone, unless another time zone is
+    specified.
+    """
+    return localtime(value, timezone).date()
+
+
+def now(tz=None):
+    """
+    Returns an aware or naive datetime.datetime, depending on param tz.
+    """
+    if tz:
+        # timeit shows that datetime.now(tz=utc) is 24% slower
+        return datetime.utcnow().replace(tzinfo=pytz.utc)
+    else:
+        return datetime.now()
+
+
+# By design, these four functions don't perform any checks on their arguments.
+# The caller should ensure that they don't receive an invalid value like None.
+
+def is_aware(value):
+    """
+    Determines if a given datetime.datetime is aware.
+
+    The concept is defined in Python's docs:
+    http://docs.python.org/library/datetime.html#datetime.tzinfo
+
+    Assuming value.tzinfo is either None or a proper datetime.tzinfo,
+    value.utcoffset() implements the appropriate logic.
+    """
+    return value.utcoffset() is not None
+
+
+def is_naive(value):
+    """
+    Determines if a given datetime.datetime is naive.
+
+    The concept is defined in Python's docs:
+    http://docs.python.org/library/datetime.html#datetime.tzinfo
+
+    Assuming value.tzinfo is either None or a proper datetime.tzinfo,
+    value.utcoffset() implements the appropriate logic.
+    """
+    return value.utcoffset() is None
+
+
+def make_aware(value, timezone=None, is_dst=None):
+    """
+    Makes a naive datetime.datetime in a given time zone aware.
+    """
+    if timezone is None:
+        timezone = get_current_timezone()
+    if hasattr(timezone, 'localize'):
+        # This method is available for pytz time zones.
+        return timezone.localize(value, is_dst=is_dst)
+    else:
+        # Check that we won't overwrite the timezone of an aware datetime.
+        if is_aware(value):
+            raise ValueError(
+                "make_aware expects a naive datetime, got %s" % value)
+        # This may be wrong around DST changes!
+        return value.replace(tzinfo=timezone)
+
+
+def make_naive(value, timezone=None):
+    """
+    Makes an aware datetime.datetime naive in a given time zone.
+    """
+    if timezone is None:
+        timezone = get_current_timezone()
+    # Emulate the behavior of astimezone() on Python < 3.6.
+    if is_naive(value):
+        raise ValueError("make_naive() cannot be applied to a naive datetime")
+    value = value.astimezone(timezone)
+    if hasattr(timezone, 'normalize'):
+        # This method is available for pytz time zones.
+        value = timezone.normalize(value)
+    return value.replace(tzinfo=None)
